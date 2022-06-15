a00e5558167b47a3bf4a803124390a6240a67373
ChristopherPerrault <christopherperrault@hotmail.com>
2022-06-15 17:39:38 -0400

    Filled Body.jsx content, added App.js comp. I/O

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Body from "./components/Photo.jsx";
+import Body from "./components/Name.jsx";
+import Body from "./components/Buttons.jsx";
+import Body from "./components/Body.jsx";
+import Body from "./components/Social.jsx";
 
 function App() {
   return (
-    <div ></div>
+    <>
+      {" "}
+      <Photo />
+      <Name />
+      <Buttons />
+      <Body />
+      <Social />
+    </>
   );
 }
 
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,1 +1,16 @@
-//
+export default function Body() {
+  return (
+    <div id="body">
+      <h2 className="body-section-title">About</h2>
+      <p className="body-text">
+        A charming Full Stack student who used to be a professional cook. Punk
+        rock singer and lover of big cats.
+      </p>
+      <h2 className="body-section-title">Interests</h2>
+      <p className="body-text">
+        Playing video games, going to concerts, resisting age-related aches and
+        pains
+      </p>
+    </div>
+  );
+}
